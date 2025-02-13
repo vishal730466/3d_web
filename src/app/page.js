1e9df529c70992e5b0ed3cebfea851ff21dd6d64
vishale <vishalingle9876@gmail.comm>
2025-02-13 15:23:55 +0530

    3

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import About from "./About/page";
-import ThreeDObject from "@/app/Swip/page"
 
 export default function Home() {
   return (
