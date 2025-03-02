aa0173217dc475eea7a5c0e39100e25d48a5ad5b
vishale <vishalingle9876@gmail.comm>
2025-03-02 09:17:18 +0530

    1

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
     <div >
      {/* <ThreeDObject/> */}
      
-     <About/>
+     {/* <About/> */}
      <Search/>
 
     </div>
--- src/app/searchboard/page.js
+++ src/app/searchboard/page.js
@@ -59,7 +59,7 @@ const Search = () => {
     <OrbitControls/>
     
                <UP/>
-               {/* <Enterbtn/> */}
+               <Enterbtn/>
             <Down/>
             <Box/>
             <ambientLight intensity={2} color={"white"}/>
